025295a3eca33c9494f772df2477a33c46c00db6
Scott Cooper <scttcper@gmail.com>
2015-12-07 11:33:04 -0800

    fix eslintrc and pull functions up in index.js

--- index.js
+++ index.js
@@ -8,9 +8,30 @@ const ms = require('ms');
 /**
  * Expose `ratelimit()`.
  */
-
 module.exports = ratelimit;
 
+function get(db, p) {
+    return new Promise(function(resolve) {
+        db.get(p, function(err, reply) {
+            resolve(reply);
+        });
+    });
+}
+
+function pttl(db, p) {
+    return new Promise(function(resolve) {
+        db.pttl(p, function(err, reply) {
+            resolve(reply);
+        });
+    });
+}
+
+function finish(ctx, next, max, n, t) {
+    ctx.set('X-RateLimit-Limit', max);
+    ctx.set('X-RateLimit-Remaining', n);
+    ctx.set('X-RateLimit-Reset', t);
+    return next();
+}
 
 /**
  * Initialize a new limiter with `opts`:
@@ -25,46 +46,23 @@ module.exports = ratelimit;
 function ratelimit(opts) {
     opts = opts || {};
 
-    function get(p) {
-        return new Promise(function(resolve) {
-            opts.db.get(p, function(err, reply) {
-                resolve(reply);
-            });
-        });
-    }
-
-    function pttl(p) {
-        return new Promise(function(resolve) {
-            opts.db.pttl(p, function(err, reply) {
-                resolve(reply);
-            });
-        });
-    }
-
-    function finish(ctx, next, n, t) {
-        ctx.set('X-RateLimit-Limit', opts.max);
-        ctx.set('X-RateLimit-Remaining', n);
-        ctx.set('X-RateLimit-Reset', t);
-        return next();
-    }
-
     return function(ctx, next) {
         let id = opts.id ? opts.id(ctx) : ctx.ip;
         if (false === id) return next();
         let name = `limit:${id}:count`;
-        return get(name).then(function(cur) {
+        return get(opts.db, name).then(function(cur) {
             let n = ~~cur;
             let ex = opts.duration || 3600000;
             let t = Date.now();
             t += ex;
             t = new Date(t).getTime() / 1000 | 0;
             if (cur !== null) {
-                return pttl(name).then(function(ex) {
+                return pttl(opts.db, name).then(function(ex) {
                     if (n - 1 >= 0) {
                         // existing user
                         opts.db.decr(name);
                         n = n - 1;
-                        return finish(ctx, next, n, t);
+                        return finish(ctx, next, opts.max, n, t);
                     }
                     // user maxed
                     ctx.set('Retry-After', t);
@@ -76,8 +74,8 @@ function ratelimit(opts) {
                     return;
                 });
             }
-            opts.db.set(name, opts.max-1, 'PX', opts.duration || 3600000, 'NX');
-            return finish(ctx, next, opts.max-1, t);
+            opts.db.set(name, opts.max - 1, 'PX', opts.duration || 3600000, 'NX');
+            return finish(ctx, next, opts.max, opts.max - 1, t);
         });
-    }
+    };
 }
