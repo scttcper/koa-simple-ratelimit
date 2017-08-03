8022f8ce9a8fb0ab43a3044c7c43551d2b3f45e2
Scott Cooper <scttcper@gmail.com>
2017-08-02 21:31:34 -0700

    upgrade lines to match koa-ratelimits new es6

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-'use strict';
 
 /**
  * Module dependencies.
  */
+
 const debug = require('debug')('koa-simple-ratelimit');
 const ms = require('ms');
 
@@ -29,6 +29,12 @@ function pttl(db, p) {
 }
 
 /**
+ * Expose `ratelimit()`.
+ */
+
+module.exports = ratelimit;
+
+/**
  * Initialize ratelimit middleware with the given `opts`:
  *
  * - `duration` limit duration in milliseconds [1 hour]
@@ -44,13 +50,12 @@ function pttl(db, p) {
  * @return {Function}
  * @api public
  */
-
-function ratelimit(opts) {
-  opts = opts || {};
-  opts.headers = opts.headers || {};
-  opts.headers.remaining = opts.headers.remaining || 'X-RateLimit-Remaining';
-  opts.headers.reset = opts.headers.reset || 'X-RateLimit-Reset';
-  opts.headers.total = opts.headers.total || 'X-RateLimit-Limit';
+function ratelimit(opts = {}) {
+  const {
+    remaining = 'X-RateLimit-Remaining',
+    reset = 'X-RateLimit-Reset',
+    total = 'X-RateLimit-Limit',
+  } = opts.headers || {};
 
   return async function ratelimiter(ctx, next) {
     const id = opts.id ? opts.id(ctx) : ctx.ip;
@@ -75,15 +80,17 @@ function ratelimit(opts) {
     t += opts.duration || 3600000;
     t = new Date(t).getTime() / 1000 || 0;
 
-    const headers = {};
-    headers[opts.headers.remaining] = opts.max - 1;
-    headers[opts.headers.reset] = t;
-    headers[opts.headers.total] = opts.max;
+    const headers = {
+      [remaining]: opts.max - 1,
+      [reset]: t,
+      [total]: opts.max,
+    };
     ctx.set(headers);
+
+    debug('remaining %s/%s %s', opts.max - 1, opts.max, id);
 
     // not existing in redis
     if (cur === null) {
-      debug('remaining %s/%s %s', opts.max - 1, opts.max, id);
       opts.db.set(name, opts.max - 1, 'PX', opts.duration || 3600000, 'NX');
       return next();
     }
@@ -92,31 +99,21 @@ function ratelimit(opts) {
     if (n - 1 >= 0) {
       // existing in redis
       opts.db.decr(name);
-      debug('remaining %s/%s %s', n - 1, opts.max, id);
-      headers[opts.headers.remaining] = n - 1;
-      ctx.set(headers);
+      ctx.set(remaining, n - 1);
       return next();
     }
     if (expires < 0) {
       debug(`${name} is stuck. Resetting.`);
-      debug('remaining %s/%s %s', opts.max - 1, opts.max, id);
       opts.db.set(name, opts.max - 1, 'PX', opts.duration || 3600000, 'NX');
       return next();
     }
     // user maxed
-    headers['Retry-After'] = t;
-    headers[opts.headers.remaining] = n;
-    ctx.set(headers);
+    ctx.set(remaining, n);
+    ctx.set('Retry-After', t);
     ctx.status = 429;
-    const retryTime = ms(expires, { long: true });
-    ctx.body = `Rate limit exceeded, retry in ${retryTime}`;
+    ctx.body = opts.errorMessage || `Rate limit exceeded, retry in ${ms(expires, { long: true })}.`;
     if (opts.throw) {
       ctx.throw(ctx.status, ctx.body, { headers: headers });
     }
   };
 }
-
-/**
- * Expose `ratelimit()`.
- */
-module.exports = ratelimit;
