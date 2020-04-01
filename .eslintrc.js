613ce0ca96190c6a1952064991b14edf09dacbc6
Scott Cooper <scttcper@gmail.com>
2020-03-31 20:36:49 -0700

    chore: upgrade dependenncies

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,10 @@ module.exports = {
     'capitalized-comments': 0,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/restrict-template-expressions': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    '@typescript-eslint/no-unsafe-call': 0,
+    '@typescript-eslint/prefer-readonly-parameter-types': 0,
+    '@typescript-eslint/no-unsafe-return': 0,
     'comma-dangle': ['error', 'always-multiline'],
   },
 };
